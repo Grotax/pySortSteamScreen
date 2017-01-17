88f6efb056494a02ae9e5736539f45089a51b185
Grot4x <benjaminbrahmer@gmail.com>
2017-01-17 20:55:01 +0100

    Another bug bites the dust

--- main.py
+++ main.py
@@ -1,24 +1,49 @@
 #!/usr/bin/env python3
 import requests
+import os
 
 knownNames = {}
 
 
-def getName(SteamID):
+def getSteamName(steamID):
     global knownNames
-    if(SteamID in knownNames):
-        return knownNames.get(SteamID)
+    if(steamID in knownNames):
+        return knownNames.get(steamID)
     else:
-        payload = {'appids': SteamID}
-        r = requests.get('https://store.steampowered.com/api/appdetails/', params=payload)
-        data = r.json()
-        knownNames[SteamID] = data[SteamID]['data']['name']
-        return knownNames.get(SteamID)
+        # Steam id max
+        if int(steamID) < 9223372036854775807:
+            payload = {'appids': steamID}
+            r = requests.get('https://store.steampowered.com/api/appdetails/', params=payload)
+            data = r.json()
+            print(data[steamID]['success'])
+            knownNames[steamID] = data[steamID]['data']['name']
+            return knownNames.get(steamID)
+        else:
+            print("Not a Steam game.")
+            return "Unkown"
+
+
+def moveFiles(steamID, name):
+    newdir = os.getcwd()+"/"+name
+    if not os.path.exists(newdir):
+        os.makedirs(newdir)
+        print(newdir+" was created.")
+    for file in os.listdir(os.getcwd()):
+        name = os.path.basename(file)
+        if name.startswith(steamID):
+            newname = newdir+"/"+os.path.basename(file)
+            print("%s --> %s" % (file, newname))
+            os.rename(file, newname)
 
 
 def main():
-    print(getName('440'))
-    print(getName('250900'))
+    for file in os.listdir(os.getcwd()):
+        name = os.path.basename(file)
+        split = name.partition("_")
+        if not split[1] == "":
+            steamID = name.partition("_")[0]
+            print(getSteamName(steamID))
+            moveFiles(steamID, getSteamName(steamID))
 
 if __name__ == '__main__':
     main()
